1fa16394915004a9e1a506b34afc3098e8af49a3
hieudoanm <hieumdoan@gmail.com>
2023-02-05 12:25:22 +0000

    Deploying to gh-pages from @ houseofmystery/themes@7e805bc6fd5595aeae7a398778808c713fe6524d 🚀

--- next-profile-astronout/_next/static/j-G5NGIiYPLWelR0-LB0k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/784-17301114bf00e076.js","static/chunks/pages/index-5825849c00d4cb96.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next-profile-card/_next/static/MSyN7WVhiuL9XKjbRH8uF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/671-821ea3efe03a3580.js","static/chunks/pages/index-469862a9623e7282.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
