7dfd14b1b545956b881f9c59b5cb149dd96fb825
hieudoanm <hieumdoan@gmail.com>
2023-02-09 18:32:52 +0700

    Fix CI

--- themes/next-blog-minimal/next.config.js
+++ themes/next-blog-minimal/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/themes/next-blog-minimal/' : '/',
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
