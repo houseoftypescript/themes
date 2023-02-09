5d02b84c1248ff79c52b78a5bf693b840fae9736
hieudoanm <hieumdoan@gmail.com>
2023-02-09 12:14:32 +0000

    Deploying to gh-pages from @ houseofmystery/themes@2616434f6f992bb27c624ec5d1759647b76adcd1 🚀

---
+++ next-store-minimal/_next/static/cRFgXCK7UboFrcUtjE9PP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0-6bc5929511554538.js","static/chunks/pages/index-2cf956be5f557dcc.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
